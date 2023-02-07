6d93eb8bb836aae49b001130846a4470f1d08b90
Tommaso Bottà <tommaso.botta@mpolo.it>
2023-02-07 10:17:30 +0100

    init prova

--- CompetizioneRobotica/src/competizionerobotica/CompetizioneRobotica.java
+++ CompetizioneRobotica/src/competizionerobotica/CompetizioneRobotica.java
@@ -11,6 +11,7 @@ public class CompetizioneRobotica {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("prova");
     }
     
 }
