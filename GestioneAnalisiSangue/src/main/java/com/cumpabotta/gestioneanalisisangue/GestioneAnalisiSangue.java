a863f1b43ad115a8a8d7b227aa89f3be5c486a0a
Tommaso Bottà <tommaso.botta@mpolo.it>
2023-02-14 10:51:00 +0100

    validazione file json rispetto allo schema

--- GestioneAnalisiSangue/src/main/java/com/cumpabotta/gestioneanalisisangue/GestioneAnalisiSangue.java
+++ GestioneAnalisiSangue/src/main/java/com/cumpabotta/gestioneanalisisangue/GestioneAnalisiSangue.java
@@ -1,12 +1,23 @@
 package com.cumpabotta.gestioneanalisisangue;
 
+import java.io.IOException;
+
+import com.cumpabotta.gestioneanalisisangue.util.JSONValidate;
+import com.github.fge.jsonschema.core.exceptions.ProcessingException;
+
 /**
  *
  * @author TommasoBotta
  */
 public class GestioneAnalisiSangue {
+    
+    private static final String JSON_FILE = "src\\main\\resources\\esami.json";
+    private static final String SCHEMA_FILE = "src\\main\\resources\\esami-schema.json";
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        try {
+            JSONValidate.validate(JSON_FILE, SCHEMA_FILE);
+        } catch (IOException | ProcessingException e) {
+        }
     }
 }
