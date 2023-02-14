beadb555597c0951cb43a4f032ec5df10fce2fa1
Tommaso Bottà <tommaso.botta@mpolo.it>
2023-02-14 10:30:19 +0100

    init esercizio 37

---
+++ GestioneAnalisiSangue/src/main/java/com/cumpabotta/gestioneanalisisangue/GestioneAnalisiSangue.java
@@ -0,0 +1,12 @@
+package com.cumpabotta.gestioneanalisisangue;
+
+/**
+ *
+ * @author TommasoBotta
+ */
+public class GestioneAnalisiSangue {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
