3792eee4058927f18745ef86b459e6f59bcb9b01
PatrickCumpa <patrick.cumpa@mpolo.it>
2023-02-23 08:52:29 +0100

    validazione file json esiti rispetto al suo schema

--- GestioneAnalisiSangue/src/main/java/com/cumpabotta/gestioneanalisisangue/GestioneAnalisiSangue.java
+++ GestioneAnalisiSangue/src/main/java/com/cumpabotta/gestioneanalisisangue/GestioneAnalisiSangue.java
@@ -4,20 +4,31 @@ import java.io.IOException;
 
 import com.cumpabotta.gestioneanalisisangue.util.JSONValidate;
 import com.github.fge.jsonschema.core.exceptions.ProcessingException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.text.ParseException;
 
 /**
  *
  * @author TommasoBotta
  */
 public class GestioneAnalisiSangue {
-    
-    private static final String JSON_FILE = "src\\main\\resources\\esami.json";
-    private static final String SCHEMA_FILE = "src\\main\\resources\\esami-schema.json";
 
-    public static void main(String[] args) {
+    private static final Path RESOURCES_DIR = Paths.get("src", "main", "resources");
+    public static final Path ESAMI_JSON = RESOURCES_DIR.resolve("esami.json");
+    private static final Path ESITI_JSON = RESOURCES_DIR.resolve("esiti.json");
+    private static final Path ESAMI_SCHEMA = RESOURCES_DIR.resolve("esami-schema.json");
+    private static final Path ESITI_SCHEMA = RESOURCES_DIR.resolve("esiti-schema.json");
+
+    public static void main(String[] args) throws ParseException {
+        
         try {
-            JSONValidate.validate(JSON_FILE, SCHEMA_FILE);
+            System.out.print("Esami: ");
+            JSONValidate.validate(ESAMI_JSON.toString(), ESAMI_SCHEMA.toString());
+            System.out.print("Esiti: ");
+            JSONValidate.validate(ESITI_JSON.toString(), ESITI_SCHEMA.toString());
         } catch (IOException | ProcessingException e) {
+            System.err.println("Errore durante la validazione del file json: " + e.getMessage());
         }
     }
 }
